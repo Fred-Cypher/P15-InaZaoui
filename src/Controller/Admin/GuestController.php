298db2d19ca21b78c2903193fb9501547e89ec28
Fred <frederique.perrier@gmail.com>
2026-03-25 12:39:25 +0100

    Create guest page

--- src/Controller/Admin/GuestController.php
+++ src/Controller/Admin/GuestController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -15,8 +17,24 @@ class GuestController extends AbstractController
     {}
 
     #[Route('/admin/guest', name: "admin_guest_index")]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('admin/guest/index.html.twig', []);
+        $page = $request->query->getInt('page', 1);
+
+        $criteria = [];
+
+        $users = $this->em->getRepository(User::class)->findBy(
+            $criteria,
+            ['id' => 'ASC'],
+            20,
+            20 * ($page -1)
+        );
+        $total = $this->em->getRepository(User::class)->count($criteria);
+
+        return $this->render('admin/guest/index.html.twig', [
+            'users' => $users,
+            'total' => $total,
+            'page' => $page
+        ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -18,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'boolean')]
     private bool $admin = false;
 
     #[ORM\Column]
@@ -35,6 +37,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(targetEntity: Media::class, mappedBy: 'user')]
     private Collection $medias;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $authorised = true;
 
     public function __construct()
     {
@@ -126,4 +131,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function eraseCredentials(): void
     {}
+
+    public function isAuthorised(): bool
+    {
+        return $this->authorised;
+    }
+
+    public function setAuthorised(bool $authorised): self
+    {
+        $this->authorised = $authorised;
+
+        return $this;
+    }
 }
