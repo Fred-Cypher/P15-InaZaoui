7d0973df7e164450344a8c52eac4396b977a91f9
Fred <frederique.perrier@gmail.com>
2026-04-15 13:37:59 +0200

    Add inversedBy to Media entity

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -15,10 +15,10 @@ class Media
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: "medias", fetch: "EAGER")]
+    #[ORM\ManyToOne(targetEntity: User::class, fetch: "EAGER", inversedBy: "medias")]
     private ?User $user = null;
 
-    #[ORM\ManyToOne(targetEntity: Album::class, fetch: "EAGER")]
+    #[ORM\ManyToOne(targetEntity: Album::class, fetch: "EAGER", inversedBy: "medias")]
     #[ORM\JoinColumn(onDelete: "CASCADE")]
     private ?Album $album = null;
 
