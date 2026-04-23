916a1c6212649501d2ed60e3c6a027b6b884a230
Fred <frederique.perrier@gmail.com>
2026-04-23 13:06:08 +0200

    Add medias in DataFixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class AppFixtures extends Fixture
 {
@@ -20,16 +21,16 @@ class AppFixtures extends Fixture
 
         public function load(ObjectManager $manager): void
     {
-        // Création d'un Admin
+        // Admin creation
         $admin = new User();
-        $admin->setEmail('admin@test.com')
+        $admin->setEmail('ina@zaoui.com')
             ->setName('Ina Admin')
             ->setAdmin(true)
             ->setAuthorised(true)
             ->setPassword($this->hasher->hashPassword($admin, 'password'));
         $manager->persist($admin);
 
-        // Création d'un invité actif
+        // Creation of active guest
         $guestActive = new User();
         $guestActive->setEmail('active@test.com')
             ->setName('Camille Actif')
@@ -38,7 +39,7 @@ class AppFixtures extends Fixture
             ->setPassword($this->hasher->hashPassword($guestActive, 'password'));
         $manager->persist($guestActive);
 
-        // Création d'un invité bloqué
+        // Creation of blocked guest
         $guestBlocked = new User();
         $guestBlocked->setEmail('blocked@test.com')
             ->setName('Claude Bloque')
@@ -47,20 +48,48 @@ class AppFixtures extends Fixture
             ->setPassword($this->hasher->hashPassword($guestBlocked, 'password'));
         $manager->persist($guestBlocked);
 
-        // Ajout de quelques médias pour l'invité actif
-        $photos = ['cookie.jpg', 'lilas.jpg', 'mezenc.jpg', 'mouton.jpg', 'ourson.jpg'];
-        foreach ($photos as $index => $fileName) {
+        // Creation of albums linked to Ina
+        $albumNames = ['Album 1', 'Album 2', 'Album 3', 'Album 4'];
+        $inaAlbums = [];
+
+        foreach ($albumNames as $name) {
+            $album = new Album();
+            $album->setName($name);
+            if (method_exists($album, 'setUser')) {
+                $album->setUser($admin);
+            }
+            $manager->persist($album);
+            $inaAlbums[] = $album;
+        }
+
+        // Add media to Ina's Portfolio (in her albums)
+        for ($i = 1; $i <= 30; $i++) {
             $media = new Media();
-            $media->setTitle("Photo test " . ($index + 1))
-                ->setPath("uploads/tests/" . $fileName)
-                ->setUser($guestActive);
+            $media->setTitle("Photo Ina " . $i)
+                ->setPath("https://picsum.photos/seed/ina" . $i . "/800/600.webp")
+                ->setUser($admin)
+                ->setAlbum($inaAlbums[array_rand($inaAlbums)]);
             $manager->persist($media);
         }
 
-        // Ajout d'un album
-        $album = new Album();
-        $album->setName('Album test');
-        $manager->persist($album);
+        // Add media for guests
+        $slugger = new AsciiSlugger();
+
+        $guests = [$guestActive, $guestBlocked];
+
+        foreach ($guests as $guest) {
+            $slug = strtolower($slugger->slug($guest->getName()));
+
+            for ($j = 1; $j <= 20; $j++) {
+                $media = new Media();
+                $media->setTitle("Photo Jeune Talent " . $guest->getName() . ' ' . $j)
+                    ->setPath("https://picsum.photos/seed/" . $slug . "-" . $j . "/800/600.webp")
+                    ->setUser($guest)
+                    ->setAlbum(null);
+
+                $manager->persist($media);
+            }
+        }
 
         $manager->flush();
     }
